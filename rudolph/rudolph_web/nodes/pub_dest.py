54641612d4852b7e6391028b45fa42357d5e8e9b
Kwon-SeungWon <ksw7384@hanyang.ac.kr>
2022-12-02 23:32:19 +0900

    Feat: Merge rudolph_web

---
+++ rudolph/rudolph_web/nodes/pub_dest.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+서버에서 목적지와 방법을 받아서 pub_dest 노드로 보내는 코드
+pub: dest_val
+e.g. 113, 0
+"""
+
+
+import rospy
+from std_msgs.msg import String
+import requests
+import json
+import os
+import sys
+
+if os.name == "nt":
+    import msvcrt
+else:
+    import tty, termios
+
+
+def get_dest_dict() -> dict:
+    URL = "http://140.238.28.123/get_dest"
+    response = requests.get(URL)
+
+    return_dict = json.loads(response.text)
+    return return_dict
+
+
+def get_dest() -> str:
+    result_dict: dict = get_dest_dict()
+    return result_dict["dest"]  # str
+
+
+def get_method() -> str:
+    result_dict: dict = get_dest_dict()
+    return result_dict["method"]  # str
+
+
+def talker():
+    if os.name != "nt":
+        settings = termios.tcgetattr(sys.stdin)
+
+    dest = get_dest()  # e.g. 113
+    method = get_method()  # e.g. 0
+
+    rospy.init_node("pub_dest")
+    pub = rospy.Publisher("dest_val", String, queue_size=10)
+    rate = rospy.Rate(10)  # 10hz
+
+    while not rospy.is_shutdown():
+        message = f"{dest}, {method}"
+        pub.publish(message)
+        rate.sleep()
+
+    if os.name != "nt":
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+    return None
+
+
+if __name__ == "__main__":
+    try:
+        talker()
+
+    except rospy.ROSInterruptException:
+        pass
