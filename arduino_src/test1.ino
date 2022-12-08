62a63cffdd768070304c82f8288aeba9821ab1bf
cho <nara00777@hanyang.ac.kr>
2022-12-08 20:02:00 +0900

    fix : int to char

--- arduino_src/test1.ino
+++ arduino_src/test1.ino
@@ -11,11 +11,13 @@
 
 
 
+
 // Number of steps per output rotation
 const int stepsPerRevolution = 200;
 
 int sw = 6;
-int state = 0;
+char state ;
+int end_message =0; //끝날 때 보내는 메세지
 
 //unsigned long prev_time = 0;
 
@@ -24,20 +26,23 @@ Stepper doorStepper(stepsPerRevolution, 5, 4, 3, 2);
 Stepper conStepper(stepsPerRevolution, 10, 9, 8, 7);
 
 
-void opendoor() {
+void opendoor() // 문 열기
+{
   doorStepper.step(200);
   delay(1000); //1초
   doorStepper.step(0);
 
 }
 
-void landbox() {
+void landbox() // 물건 내리기
+{
   conStepper.step(600);
   delay(1000);
   conStepper.step(0);
 }
 
-void closedoor() {
+void closedoor() // 문 닫기
+{
   doorStepper.step(-200);
   delay(1000);
   doorStepper.step(0);
@@ -48,7 +53,7 @@ void setup()
 
   Serial.begin(9600);
   delay(20);
-  state = 0;
+
 
   // set the speed at 20 rpm:
   doorStepper.setSpeed(20);
@@ -66,8 +71,12 @@ void loop()
     //unsigned long now_time = millis();
 
     state = Serial.read();
+    //Serial.write(end_message);
+    end_message=0;
 
     int button_state = digitalRead(sw);
+
+    delay(100);
 
     /*if(now_time - prev_time >=500)
     {
@@ -79,23 +88,24 @@ void loop()
 
 
 
-    if (state == 0)
+    if (state == 'a') //-> 기본 상태
     {
       continue;
     }
 
 
-    if (state == 1)
+    if (state == 'b') //-> 경유지 동작
     {
       opendoor();
       if (button_state == 0)
       {
         closedoor();
         state = 0;
+        end_message=1;
       }
     }
 
-    if (state == 2)
+    if (state == 'c') //->도착지(직접 수령) 동작
     {
       opendoor();
       landbox();
@@ -103,40 +113,16 @@ void loop()
       {
         closedoor();
         state = 0;
+        end_message=1;
       }
     }
-    if (state == 3)
+    if (state == 'd') //->state = 3 ->도착지(비대면 수령)
     {
       opendoor();
       landbox();
       closedoor();
       state = 0;
+      end_message=1;
     }
   }
 }
-
-
-//state = 0 -> 기본, state = 1 -> 경유지, state = 2 ->도착지(직접 수령) , state = 3 ->도착지(비대면 수령)
-
-/*#sudo code
-
-  open_door()
-  land_box()
-  close_door()
-
-
-  while True:
-    state = 시리얼()
-    sleep.(1)
-
-    if state == 0:
-        # 경유지
-        open_door()
-        if button == 1:
-            close_door()
-
-    if state == 1:
-        # 도착지
-        open_door()
-        land_box()
-        close_door()*/
