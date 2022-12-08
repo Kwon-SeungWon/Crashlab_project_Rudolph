0fadb1c2f24892ed484151a471d88ce8a2fa6665
Kwon-SeungWon <ksw7384@hanyang.ac.kr>
2022-12-08 18:03:11 +0900

    odom fixed

--- rudolph_params/src/publish_params.py
+++ rudolph_params/src/publish_params.py
@@ -9,7 +9,7 @@ See doc at http://wiki.ros.org/rospy/Overview/Parameter%20Server
 import rospy
 
 # Robot parameter values
-ROBOT_TICKS_PER_REV = 228.0  # 24:1 worm, 26:10 spur, 11 pole encoder magnet
+ROBOT_TICKS_PER_REV = 400.0  # 24:1 worm, 26:10 spur, 11 pole encoder magnet
 ROBOT_WHEEL_CIRCUMFERENCE = 0.213  # meters
 ROBOT_TICKS_PER_METER = int(ROBOT_TICKS_PER_REV / ROBOT_WHEEL_CIRCUMFERENCE)
 ROBOT_TRACK_WIDTH = 0.4  # Wheel Separation Distance (meters)
@@ -19,8 +19,8 @@ ROBOT_MIN_X_VEL = 0.01  # Minimum x velocity robot can manage (m/s)
 ROBOT_MAX_X_VEL = 5.0  # Maximum x velocity robot can manage (m/s)
 ROBOT_MIN_Z_VEL = 0.01  # Minimum theta-z velocity robot can manage (rad/s)
 ROBOT_MAX_Z_VEL = 5.0  # Maximum theta-z velocity robot can manage (rad/s)
-ROBOT_MTR_KP = 1.1  # Proportional coeff
-ROBOT_MTR_KD = 0.5  # Derivative coeff
+ROBOT_MTR_KP = 2.2  # Proportional coeff
+ROBOT_MTR_KD = 1.5  # Derivative coeff
 ROBOT_MTR_MAX_PID_TRIM = 20 # Max allowable value for PID trim term
 
 # end points of segments of piecewise linear curve in descending order
