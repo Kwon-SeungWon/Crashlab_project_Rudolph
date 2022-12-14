59b70666359ee171d460ed73ccf698299a080da9
ydoo123 <ydoo123@gmail.com>
2022-12-14 23:59:09 +0900

    Refacotor: make code clean

--- arduino_src/arduino_door_convey_led.ino
+++ arduino_src/arduino_door_convey_led.ino
@@ -20,7 +20,7 @@
 #endif
 
 #define ledPIN 2
-#define NUMPIXELS 40 //아두이노에 부착된 neopixel 갯수
+#define NUMPIXELS 55 //아두이노에 부착된 neopixel 갯수
 
 #include <time.h>
 
@@ -139,122 +139,118 @@ void setup()
 }
 
 
-void loop()
-{ while (true)
-  { now = millis();
-    time_interval = now - past;
-    if (time_interval > 1000)
-    {
-      pixels.clear();
-      for (int i = 1; i < NUMPIXELS; i++)
-      {
-
-
-        int randNumber = random(1, 3); //led 갯수가 NUMPIXELS(현재:44) 빨간색 생성
-        if (randNumber == 1) {
-          pixels.setPixelColor(i, pixels.Color(10, 0, 0));
-        }
-        else
-        {
-          pixels.setPixelColor(i, pixels.Color(0, 10, 0));
-        }
-      }
-      pixels.show();
-      past = now;
+void turn_on_led(){
+  pixels.clear();
+  for (int i = 1; i < NUMPIXELS; i++)
+  {
+    int randNumber = random(1, 3); //led 갯수가 NUMPIXELS(현재:44) 빨간색 생성
+    if (randNumber == 1) {
+      pixels.setPixelColor(i, pixels.Color(10, 0, 0));
     }
-    state = Serial.read(); //라파 시리얼 받기
-    //Serial.write(end_message);
-    end_message = 0;
-
-    int button_state = digitalRead(sw); //버튼 상태. 풀업상태이므로 기본 1, 누를 때 0
-
-    delay(10);
-
-
-    if (state == 'a') //-> 기본 상태
+    else
     {
-      continue;
+      pixels.setPixelColor(i, pixels.Color(0, 10, 0));
     }
+  }
+  pixels.show();
+}
 
 
-    if (state == 'b' && mid_fin == 0) // 경유지 동작 -> 문이 열리고 버튼을 누르면 문이 닫힘. 이후 라파에 end_message 전송
+void mid_point()
+{
+  if (main_process == 0)
     {
-      if (main_process == 0)
-      {
-        opendoor();
-      }
-      else if (main_process == 1)
-      { while (1)
-        {
-          int button_state = digitalRead(sw);
-          delay(10);
-          if (button_state == 0)
-          {
-            closedoor();
-            state = 'a';
-            //end_message=0;
-            for (int i = 0; i < 6; i++)
-            {
-              Serial.println('1');
-              delay(10);
-              i++;
-            }
-            mid_fin = 1;
-          }
-        }
-      }
+      opendoor();
       main_process = 1;
     }
 
-    if (state == 'c' && fin_fin == 0) // 도착지(직접 수령) 동작 -> 문이 열리고 컨베이어가 작동. 버튼이 눌리면 문이 닫히고 라파에  end_message 전송
+  if (main_process == 1)
+  {
+    while (1)
     {
-      if (main_process == 1)
+      int button_state = digitalRead(sw);
+      delay(10);
+      if (button_state == 0)
       {
-        opendoor();
-        landbox();
-      }
-      if (main_process == 2)
-      { while (1)
-        { delay(10);
-          int button_state = digitalRead(sw);
-          if (button_state == 0)
-          {
-            closedoor();
-            state = 'a';
-            //end_message=1;
-            for (int i = 0; i < 6; i++)
-            {
-              Serial.println('2');
-              delay(10);
-              i++;
-            }
-            fin_fin = 1;
-          }
-        }
-      }
-      main_process = 2;
-    }
-    if (state == 'd' && fin_fin == 0) // 도착지(비대면 수령) 문이 열리고 컨베이어가 작동. 이후 문이 닫히고 라파에 end_message 전송
-    {
-      if (main_process == 1)
-      {
-        opendoor();
-        landbox();
         closedoor();
-      }
-      if (main_process == 2)
-      {
-        state = 'a';
-        //end_message=2;
+
         for (int i = 0; i < 6; i++)
         {
-          Serial.println('3');
+          Serial.println('1');
           delay(10);
-          i++;
         }
-        fin_fin = 1;
       }
-      main_process = 2;
     }
+  }
+}
+
+
+void final_point_direct()
+{
+  opendoor();
+
+  while (1)
+  {
+    int button_state = digitalRead(sw);
+    delay(10);
+
+    if (button_state == 0)
+    {
+      closedoor();
+      for (int i = 0; i < 6; i++)
+      {
+        Serial.println('2');
+        delay(10);
+      }
+    }
+  }
+}
+
+
+void final_point_untect()
+{
+  opendoor();
+  landbox();
+  closedoor();
+
+  for (int i = 0; i < 6; i++)
+  {
+    Serial.println('3');
+    delay(10);
+  }
+}
+
+
+void loop()
+{
+  now = millis();
+  time_interval = now - past;
+
+  if (time_interval > 1000)
+  {
+    turn_on_led();
+    past = now;
+  }
+
+  state = Serial.read(); //라파 시리얼 받기
+  //int button_state = digitalRead(sw); //버튼 상태. 풀업상태이므로 기본 1, 누를 때 0 
+  delay(10);
+
+  if (state == 'b' && mid_fin == 0) // 경유지 동작 -> 문이 열리고 버튼을 누르면 문이 닫힘. 이후 라파에 end_message 전송
+  {
+    mid_point();
+    mid_fin = 1;
+  }
+
+  if (state == 'c' && fin_fin == 0) // 도착지(직접 수령) 동작 -> 문이 열리고 컨베이어가 작동. 버튼이 눌리면 문이 닫히고 라파에  end_message 전송
+  {
+    final_point_untect();
+    fin_fin = 1;
+  }
+
+  if (state == 'd' && fin_fin == 0) // 도착지(비대면 수령) 문이 열리고 컨베이어가 작동. 이후 문이 닫히고 라파에 end_message 전송
+  {
+    final_point_direct();
+    fin_fin = 1;
   }
 }
